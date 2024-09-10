4adfa6dcfa973fac9e3ca93965ca041b4243f694
eringrand <eringrand@gmail.com>
2024-09-10 12:55:59 -0400

    clean up to not use "resume only" flag

--- render_cv.r
+++ render_cv.r
@@ -8,36 +8,26 @@
 write_pdfs <- function(rmd_name, html_name, pdf_name, resume_only = FALSE) {
   # Knit the HTML version
   rmarkdown::render(rmd_name,
-                    params = list(pdf_mode = FALSE,
-                                  resume_only = resume_only),
+                    params = list(pdf_mode = FALSE),
                     output_file = html_name)
 
   # Knit the PDF version to temporary html location
   tmp_html_cv_loc <- fs::file_temp(ext = ".html")
 
   rmarkdown::render(rmd_name,
-                    params = list(pdf_mode = TRUE,
-                                  resume_only = resume_only),
+                    params = list(pdf_mode = TRUE),
                     output_file = tmp_html_cv_loc)
 
   # Convert to PDF using Pagedown
   pagedown::chrome_print(input = tmp_html_cv_loc,
                          output = pdf_name)
 
-  rmarkdown::render(rmd_name,
-                    params = list(pdf_mode = TRUE,
-                                  resume_only = resume_only),
-                    output_file = tmp_html_cv_loc)
-
-  # Convert to PDF using Pagedown
-  pagedown::chrome_print(input = tmp_html_cv_loc,
-                         output = pdf_name)
 }
 
 
 write_pdfs("resume.rmd", "resume.html", "resume.pdf", resume_only = TRUE)
-write_pdfs("cv.rmd", "cv.html", "cv.pdf", resume_only = FALSE)
-#write_pdfs("cover_letter_headspace.rmd", "cover_letter_headspace.html", "cover_letter_headspace.pdf", resume_only = FALSE)
+# write_pdfs("cv.rmd", "cv.html", "cv.pdf", resume_only = FALSE)
+# write_pdfs("cover_letter_headspace.rmd", "cover_letter_headspace.html", "cover_letter_headspace.pdf", resume_only = FALSE)
 
 # library(pdftools)
 #
